c0c87a623fb32792c3830bba03cfc33feefda62e
Victor Opusculo <victor.ventura@uol.com.br>
2024-03-26 20:01:52 -0300

    Página de login de admin redireciona para painel de admin se já estiver logado

--- app/Admin/Login.php
+++ app/Admin/Login.php
@@ -2,6 +2,8 @@
 
 namespace VictorOpusculo\Parlaflix\App\Admin;
 
+use VictorOpusculo\Parlaflix\Lib\Helpers\URLGenerator;
+use VictorOpusculo\Parlaflix\Lib\Helpers\UserTypes;
 use VictorOpusculo\PComp\Component;
 use VictorOpusculo\PComp\HeadManager;
 
@@ -12,7 +14,15 @@ final class Login extends Component
 {
     protected function setUp()
     {
-        HeadManager::$title = "Log-in de administrador";    
+        HeadManager::$title = "Log-in de administrador"; 
+        
+        session_name('parlaflix_admin_user');
+        session_start();
+        if (($_SESSION['user_type'] ?? '') === UserTypes::administrator)
+        {
+            header('location:' . URLGenerator::generatePageUrl('/admin/panel'), true, 303);
+            exit;
+        }
     }
 
     protected function markup(): Component|array|null
